d3a2633f206cb49c219c3ead66ae84fa1353454e
Aleksandr Khan <manchig1990@gmail.com>
2024-03-07 17:42:55 +1000

    add Task 1 Method int and void

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ************************************************************************************************
+// Задача 1: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// Пример:
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// int Summa(int num)
+// {
+//     int result = 0;
+//     for (int i = 1; i <= num; i++)
+//         result += i;
+//     return result;
+// }
+
+// // void Summa2(int num)
+// // {
+// //     int result = 0;
+// //     for (int i = 1; i <= num; i++)
+// //         result += i;
+// //     System.Console.WriteLine($"Вы ввели: {num}, сумма всех чисел равна: {result}");
+// // }
+
+
+// System.Console.WriteLine("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// int result = Summa(num);
+// System.Console.WriteLine($"Вы ввели: {num}, сумма всех чисел равна: {result}");
+// // Summa2(num);
