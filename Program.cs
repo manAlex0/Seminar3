a2bc75d12e91908821ad00f7062fb48fb3451db6
Aleksandr Khan <manchig1990@gmail.com>
2024-03-07 18:21:33 +1000

    add Task 2

--- Program.cs
+++ Program.cs
@@ -7,25 +7,62 @@
 // 4 -> 10
 // 8 -> 36
 
-int Summa(int num)
-{
-    int result = 0;
-    for (int i = 1; i <= num; i++)
-        result += i;
-    return result;
-}
-
-// void Summa2(int num)
+// int Summa(int num)
 // {
 //     int result = 0;
 //     for (int i = 1; i <= num; i++)
 //         result += i;
-//     System.Console.WriteLine($"Вы ввели: {num}, сумма всех чисел равна: {result}");
+//     return result;
 // }
 
+// // void Summa2(int num)
+// // {
+// //     int result = 0;
+// //     for (int i = 1; i <= num; i++)
+// //         result += i;
+// //     System.Console.WriteLine($"Вы ввели: {num}, сумма всех чисел равна: {result}");
+// // }
+
+
+// System.Console.WriteLine("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// int result = Summa(num);
+// System.Console.WriteLine($"Вы ввели: {num}, сумма всех чисел равна: {result}");
+// // Summa2(num);
+
+
+
+// ************************************************************************************************
+// Задача 2: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// Пример:
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// int QuantityNum(int numbers)
+// {
+//     int result = 0;
+//     while (numbers > 0)
+//     {
+//         numbers /= 10;
+//         result++;
+//     }
+//     return result;
+// }
+
+void QuantityNum2(int numbers)
+{
+    int check = 0;
+    while (numbers > 0)
+        {
+        numbers /= 10;
+        check++;
+        }
+    System.Console.WriteLine($"В вышем числе состовляет {check} цифр");
+}
 
 System.Console.WriteLine("Введите число: ");
-int num = int.Parse(Console.ReadLine()!);
-int result = Summa(num);
-System.Console.WriteLine($"Вы ввели: {num}, сумма всех чисел равна: {result}");
-// Summa2(num);
+int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"В вышем числе: {num} состовляет {QuantityNum(num)} цифр");
+QuantityNum2(num);
