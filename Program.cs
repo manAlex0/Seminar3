0c9f536d6f7f8d00df07d96efce497a9e319f947
Aleksandr Khan <manchig1990@gmail.com>
2024-03-08 01:26:49 +1000

    edit Home Work 2

--- Program.cs
+++ Program.cs
@@ -187,8 +187,8 @@ int SummNumb(int num)
     int summ = 0;
     while (num > 0)
     {
-        summ = summ + (num%10);
-        num/=10;
+        summ = summ + (num % 10);
+        num /= 10;
     }
     return summ;
 }
