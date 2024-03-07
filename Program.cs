755dbd066c550be2f786cc16825982513c8ff78f
Aleksandr Khan <manchig1990@gmail.com>
2024-03-07 20:15:43 +1000

    add Home Task 1

--- Program.cs
+++ Program.cs
@@ -133,16 +133,67 @@
 // int[] array = new int[8];
 // RandomArray(array);
 
-int[] FillArray()
+// int[] FillArray()
+// {
+//     int[] array = new int[8];
+//     Random random = new Random();
+//     for (int i = 0; i < 8; i++)
+//     {
+//         array[i] = random.Next(2);
+//     }
+//     return array;
+// }
+
+// int[] array = FillArray();
+// System.Console.WriteLine(string.Join(", ", array));
+
+
+
+// *****************************************************************************Практическое задание********************************
+
+// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Math.Pow юзать нельзя.
+// Пример:
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int MathPow(int a, int b)
 {
-    int[] array = new int[8];
-    Random random = new Random();
-    for (int i = 0; i < 8; i++)
+    int result = 1;
+    for (int i = 0; i < b; i++)
     {
-        array[i] = random.Next(2);
+        result *=a;
     }
-    return array;
+    return result;
 }
 
-int[] array = FillArray();
-System.Console.WriteLine(string.Join(", ", array));
+System.Console.WriteLine("Введите число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите степень: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+int result = MathPow(firstNumber, secondNumber);
+System.Console.WriteLine($"Возводим число {firstNumber} в натуральную степень {secondNumber}. Получаем: {result}");
+
+
+// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Пример:
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+
+// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// Пример:
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+
+// Задача 4: HARD Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.
+// Пример:
+// 456 -> 3
+// 0 -> 1
+// 89,126 -> 5
+// 0,001 -> 4
+// 000000 -> 1
